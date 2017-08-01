8d9f19ca2e210ed1c1ab83b9bba65e3ba7bfc636
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-08-01 11:13:48 -0400

    serial computation of nested loops

--- prog-1.cpp
+++ prog-1.cpp
@@ -1,12 +1,28 @@
 #include <sstream>
 #include <iostream>
 #include <omp.h>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-#pragma omp parallel //prompts compiler to execute the next block of code in braces in parallel
-    {
-        printf("hello from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+    const int size = 10000;
+    std::vector<double> a;
+
+    a.resize(size);
+
+    double start = omp_get_wtime(); // returns present timestamp in double
+
+    for (int i = 0; i < size; i++) {
+        a[i] = 0;
+        for (int j = 0; j < size; j++)
+        {
+            a[i] += sqrt(sqrt(i+j));
+        }
     }
+
+    double stop = omp_get_wtime();
+    std::cout << "time: " << stop - start << std::endl;
     return 0;
 }
