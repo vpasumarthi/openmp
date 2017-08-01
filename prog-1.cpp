7f276b29ffe63a0abb4ae031e963dfdb3cbb784c
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-08-01 10:59:33 -0400

    pass string stream object to parallel threads

--- prog-1.cpp
+++ prog-1.cpp
@@ -1,8 +1,14 @@
+#include <sstream>
 #include <iostream>
 #include <omp.h>
 
 int main()
 {
-    std::cout << "OpenMP will use " << omp_get_max_threads() << " threads" << std::endl;
+#pragma omp parallel //prompts compiler to execute the next block of code in braces in parallel
+    {
+        std::stringstream ss;
+        ss << "OpenMP will use " << omp_get_max_threads() << " threads." << std::endl;
+        std::cout << ss.str();
+    }
     return 0;
 }
