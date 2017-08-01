5c8df196ca5372579beac3619054debaa757dfa4
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-08-01 14:30:26 -0400

    runtime comparison between static and dynamic scheduling

--- prog-1.cpp
+++ prog-1.cpp
@@ -14,9 +14,10 @@ double two_body_energy(int i, int j)
 
 int main()
 {
-    const int nbodies = 1000;
+    const int nbodies = 100000;
     double energy = 0.0;
-
+    
+    double start = omp_get_wtime(); // returns present timestamp in double
 #pragma omp parallel for schedule(dynamic) reduction(+:energy)
     for (int i = 0; i < nbodies; i++) {
         for (int j = i+1; j < nbodies; j++) {
@@ -24,6 +25,9 @@ int main()
             energy += eij;
         }
     }
+
+    double stop = omp_get_wtime();
+    std::cout << "time: " << stop - start << std::endl;
 
     printf("energy = %.5lf\n", energy);
     //std::cout << "energy = " << energy << std::endl;
